bbccaf39e0b7b3d0619b856551ea6654f1041800
Manuela Pina Coronado <abdipina11@gmail.com>
2024-12-22 12:35:00 -0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+
+--CREATE DATABASE sistema_stock;
+--USE sistema_stock;
+
+--CREATE TABLE usuarios (
+--id INT IDENTITY(1,1) PRIMARY KEY, 
+--nombre VARCHAR(100) NOT NULL,
+--email VARCHAR(100) NOT NULL UNIQUE,
+--telefono VARCHAR(20) NOT NULL,
+--contrasena VARCHAR(255) NOT NULL,
+--fecha_registro DATETIME DEFAULT GETDATE()
+--);
+
+SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'usuarios';
+
+
+
+
